a267e3fc64b5868adb46ea34f75220d4e0ab154f
rskrydge <rskrydge@itlab-studio.com>
2025-01-30 11:57:21 +0200

    add jwt

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,43 +2,156 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\UnprocessableEntityHttpException;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-#[Route('/api/test')]
+#[Route('/api/v1')]
 class TestController extends AbstractController
 {
 
-    #[Route('/get', name: 'app_test_get', methods: ['GET'])]
+    public const USERS_DATA = [
+        [
+            'id'    => '1',
+            'email' => 'test1@gmail.com',
+            'name'  => 'John1'
+        ],
+        [
+            'id'    => '2',
+            'email' => 'test2@gmail.com',
+            'name'  => 'John2'
+        ],
+        [
+            'id'    => '3',
+            'email' => 'test3@gmail.com',
+            'name'  => 'John3'
+        ],
+        [
+            'id'    => '4',
+            'email' => 'test4@gmail.com',
+            'name'  => 'John4'
+        ],
+        [
+            'id'    => '5',
+            'email' => 'test5@gmail.com',
+            'name'  => 'John5'
+        ],
+        [
+            'id'    => '6',
+            'email' => 'test6@gmail.com',
+            'name'  => 'John6'
+        ],
+        [
+            'id'    => '7',
+            'email' => 'test7@gmail.com',
+            'name'  => 'John7'
+        ],
+    ];
+
+    #[Route('/users', name: 'app_collection_users', methods: ['GET'])]
     #[IsGranted("ROLE_ADMIN")]
-    public function get(Request $request): JsonResponse
+    public function getCollection(): JsonResponse
     {
-        /** @var User $user */
-        $user = $this->getUser();
-
-        dd($user);
-
-        $queryParams = $request->query->all();
-
-        return new JsonResponse($queryParams);
+        return new JsonResponse([
+            'data' => self::USERS_DATA
+        ], Response::HTTP_OK);
     }
 
-    #[Route('/post', name: 'app_test_post', methods: ['POST'])]
-    public function post(Request $request): JsonResponse
-    {
-        $requestBody = json_decode($request->getContent(), true);
-
-        return new JsonResponse($requestBody);
-    }
-
-    #[Route('/get-item/{id}', name: 'app_test_get_item', methods: ['GET'])]
+    #[Route('/users/{id}', name: 'app_item_users', methods: ['GET'])]
     public function getItem(string $id): JsonResponse
     {
-        return new JsonResponse();
+        $userData = $this->findUserById($id);
+
+        return new JsonResponse([
+            'data' => $userData
+        ], Response::HTTP_OK);
+    }
+
+    #[Route('/users', name: 'app_create_users', methods: ['POST'])]
+    public function createItem(Request $request): JsonResponse
+    {
+        $requestData = json_decode($request->getContent(), true);
+
+        if (!isset($requestData['email'], $requestData['name'])) {
+            throw new UnprocessableEntityHttpException("name and email are required");
+        }
+
+        // TODO check by regex
+
+        $countOfUsers = count(self::USERS_DATA);
+
+        $newUser = [
+            'id'    => $countOfUsers + 1,
+            'name'  => $requestData['name'],
+            'email' => $requestData['email']
+        ];
+
+        // TODO add new user to collection
+
+        return new JsonResponse([
+            'data' => $newUser
+        ], Response::HTTP_CREATED);
+    }
+
+    #[Route('/users/{id}', name: 'app_delete_users', methods: ['DELETE'])]
+    public function deleteItem(string $id): JsonResponse
+    {
+        $this->findUserById($id);
+
+        // TODO remove user from collection
+
+        return new JsonResponse([], Response::HTTP_NO_CONTENT);
+    }
+
+    #[Route('/users/{id}', name: 'app_update_users', methods: ['PATCH'])]
+    public function updateItem(string $id, Request $request): JsonResponse
+    {
+        $requestData = json_decode($request->getContent(), true);
+
+        if (!isset($requestData['name'])) {
+            throw new UnprocessableEntityHttpException("name is required");
+        }
+
+        $userData = $this->findUserById($id);
+
+        // TODO update user name
+
+        $userData['name'] = $requestData['name'];
+
+        return new JsonResponse(['data' => $userData], Response::HTTP_OK);
+    }
+
+    /**
+     * @param string $id
+     * @return string[]
+     */
+    public function findUserById(string $id): array
+    {
+        $userData = null;
+
+        foreach (self::USERS_DATA as $user) {
+            if (!isset($user['id'])) {
+                continue;
+            }
+
+            if ($user['id'] == $id) {
+                $userData = $user;
+
+                break;
+            }
+
+        }
+
+        if (!$userData) {
+            throw new NotFoundHttpException("User with id " . $id . " not found");
+        }
+
+        return $userData;
     }
 
 }
